616bd125639e3c76889fbf9f47fbe829b6ed25e2
the-old-eagle <2523977224@qq.com>
2022-08-26 15:30:27 +0800

    test

--- 09-Data-Structures-Operators/starter/Set&map.js
+++ 09-Data-Structures-Operators/starter/Set&map.js
@@ -29,7 +29,7 @@ console.log("-------------------------------------------");
 const rest = new Map();
 rest.set("name", "Classico Italiano");
 rest.set(1, "Firenze,Italy");
-console.log(rest.set(2, "Lisbon,Portugal"));
+// console.log(rest.set(2, "Lisbon,Portugal"));
 
 rest
   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
@@ -37,37 +37,6 @@ rest
   .set("close", 23)
   .set(true, "We are open :D")
   .set(false, "We are closed :(");
-console.log("size before delete: " + rest.size);
 
 console.log(rest.get("name"));
-console.log(rest.get(true));
-console.log(rest.get(1));
-
-console.log("----------------------------");
-const time = 8;
-console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
-
-console.log("--------------------------------------");
-console.log(rest.has("categories"));
-rest.delete(2);
-console.log(rest);
-console.log("size after delete: " + rest.size);
-
-// console.log("=======================================================");
-// rest.clear();
-// console.log(rest);
-
-console.log("=======================================================");
-
-const arr = [1, 2];
-rest.set(arr, "Test");
-rest.set(document.querySelector("h1"), "Heading");
-console.log(rest);
-console.log(rest.size);
-
-console.log(rest.get(arr));
-
-console.log("--------------------------------------------------------------");
-const question = new Map([
-  ["question", "What is the best programming language in the world?"],
-]);
+console.log(rest.get("true"));
