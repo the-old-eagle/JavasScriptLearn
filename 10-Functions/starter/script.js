c0afbe127a5a3b83938b56a2ef12763081a6ed07
the-old-eagle <2523977224@qq.com>
2022-09-23 19:56:33 +0800

    test

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,10 @@
 "use strict";
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}:${value}`);
+});
--- 10-Functions/starter/闭包的理解.js
+++ 10-Functions/starter/闭包的理解.js
@@ -1,8 +1,8 @@
 "use strict";
-const boardPassengers = function(n, wait) {
+const boardPassengers = function (n, wait) {
   const perGroup = n / 3;
 
-  setTimeout(function() {
+  setTimeout(function () {
     console.log(`We are now boarding all ${n} passengers`);
     console.log(`There are 3 groups,each with ${perGroup} passengers`);
   }, wait * 3);
