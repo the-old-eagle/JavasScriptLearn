df8ea8f5af28af87703352a7ac602a594ac63453
the-old-eagle <2523977224@qq.com>
2022-11-28 16:40:44 +0800

    test

---
+++ 13-Advanced-DOM-Bankist/starter/DOMTraversing.js
@@ -0,0 +1,28 @@
+//DOM Traversing
+// const h1=document.querySelector('h1')
+
+//Going downwards:child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+// Going upwards:parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-primary)';
+h1.closest('h1').style.background = 'var(--gradient-secondary)';
+
+// Going sideways:sublings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el != h1) {
+    el.style.transform = 'scale(0.5)';
+  }
+});
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,6 +30,31 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//////////////////////////////////////////////////////////
+// Page navigation
+
+// low efficient way
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// use bubble way
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
 // Creating and inserting elements
 const header = document.querySelector('header');
 const message = document.createElement('div');
@@ -138,4 +163,33 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 5000);
 // document.querySelector('.nav').addEventListener('click', function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log('Nav', e.target);
+// });
+
+// //DOM Traversing
+// // const h1=document.querySelector('h1')
+
+// //Going downwards:child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+
+// // Going upwards:parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-primary)';
+// h1.closest('h1').style.background = 'var(--gradient-secondary)';
+
+// // Going sideways:sublings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el != h1) {
+//     el.style.transform = 'scale(0.5)';
+//   }
 // });
